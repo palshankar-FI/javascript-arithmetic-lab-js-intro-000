f953080eda6cf06c581f50e59fe81231e67944bd
PS <palshankar@outlook.com>
2019-03-25 03:07:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function subtract(a, b) {
   return(a - b);
 }
 
-it('multiply(a, b) multiplies two numbers and returns the result', function() {
-  expect(multiply(a, b)).toEqual(a * b)
-})
+function multiply(a, b) {
+  return(a * b);
+}
 
 it('divide(a, b) divides a by b and returns the result', function() {
   expect(divide(a, b)).toEqual(a / b)
